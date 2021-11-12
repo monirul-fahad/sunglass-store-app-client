7f9702ccb823574cd8e00a98b9aa31b79cabbf2d
Monirul Fahad <immonirulfahad@gmail.com>
2021-11-12 23:02:05 +0600

    test

--- src/Pages/Dashboard/Reviews/Reviews.js
+++ src/Pages/Dashboard/Reviews/Reviews.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import axios from "axios";
+import axios from "axios";
 import { useForm } from "react-hook-form";
 import useAuth from "../../../hooks/useAuth";
 import Sidebar from "../../Sidebar/Sidebar";
@@ -9,6 +9,12 @@ const Reviews = () => {
   const { register, handleSubmit, reset } = useForm();
   const onSubmit = (data) => {
     console.log(data);
+    axios.post("http://localhost:5000/reviews", data).then((res) => {
+      if (res.data.insertedId) {
+        alert("Review added successfully");
+        reset();
+      }
+    });
   };
   return (
     <div className="row">
@@ -19,7 +25,10 @@ const Reviews = () => {
       >
         <h2>Give Review On Your Used Glass</h2>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <input {...register("name")} defaultValue={user.displayName} />
+          <input
+            {...register("name", { required: true })}
+            defaultValue={user.displayName}
+          />
           <input
             {...register("productName", { required: true })}
             placeholder="Sunglass Name"
