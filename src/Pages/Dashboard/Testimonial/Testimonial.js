4953163b9b2a564d47025e11efa80c5b1096a993
Monirul Fahad <immonirulfahad@gmail.com>
2021-11-12 23:50:14 +0600

    review component

--- src/Pages/Dashboard/Testimonial/Testimonial.js
+++ src/Pages/Dashboard/Testimonial/Testimonial.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-const Testimonial = () => {
+const Testimonial = ({ review }) => {
   return (
-    <div>
-      <h2>hsdfjdsflk</h2>
+    <div className="col-md-6 col-lg-4">
+      <div style={{ border: "1px solid gray" }} className="p-4 rounded mb-5">
+        <div>
+          <h3>{review.productName}</h3>
+          <p>Review by: {review.name}</p>
+        </div>
+
+        <p>{review.description} </p>
+      </div>
     </div>
   );
 };
